33aa11188447e5fa53abedb62c6ebdbdd554bc60
harpreet singh <1041harpreet@gmail.com>
2023-08-17 07:55:56 +0530

    a

---
+++ array/revise/kadane_algo.cpp
@@ -0,0 +1,10 @@
+// long long maxSubarraySum(int arr[], int n){
+//          long long max_ending_here = arr[0];
+//          long long max_so_far = arr[0];
+
+//     for (int i = 1; i < n; i++) {
+//         max_ending_here = max(static_cast<long long>(arr[i]), max_ending_here + arr[i]);
+//         max_so_far = max(max_so_far, max_ending_here);
+//     }
+//     return max_so_far;
+//     }
